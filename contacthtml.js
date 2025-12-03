0a1b91ac1bca856ceb16ad45088a4fbf40dc2b2c
Akashinme <A00037291@myTUDublin.ie>
2025-12-03 14:53:42 +0000

    Added my Javascript for contact html
    Add event listener to change input field based on language selection.

--- contacthtml.js
+++ contacthtml.js
@@ -1,1 +1,36 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdown = document.querySelector("select[name=' langdropdown ']");
+  const inputField = document.getElementById("ddepartment");
+
+  if (!dropdown || !inputField) return;
+
+  dropdown.addEventListener("change", function () {
+    const value = this.value;
+
+
+    switch (value) {
+      case "eng":
+        inputField.placeholder = "Enter your email";
+        inputField.type = "email";
+        inputField.required = true;
+        break;
+
+      case "spa":
+        inputField.placeholder = "Enter your phone number";
+        inputField.type = "tel";
+        inputField.required = true;
+        break;
+      
+      
+      case "eng":
+        inputField.placeholder = "Enter your contact info";
+        inputField.type = "none";
+        inputField.required = true;
+        break;
+
+
+    }
+  });
+});
+
